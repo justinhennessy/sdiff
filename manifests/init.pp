225d8e1428b1a052e541f9ad6dcc8efa077d558c
Justin Hennessy <justhennessy@gmail.com>
2014-03-18 08:07:46 +1000

    Updated module to be able to pass in color true/false

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
-# == Class: cdiff
+# == Class: sdiff
 #
-# This class adds a wrapper for the diff command puppet uses as well as puppet.conf
-# updates that are needed for puppet to use it on each node including the master.
+# This class adds a wrapper for the diff command puppet uses.
 # It introduces the ability to suppress diff output, useful for encrypted data.
 #
 # This module requires changes to the puppet.conf file that is on the agents,
@@ -20,11 +19,16 @@
 #   * This is thus destructive and should be used with care.
 #   Defaults to <tt>present</tt>.
 #
+# [*color*]
+#   Boolean. Optionally color line in diff output (red for deletions, green for
+#   insertions and yellow for supression).
+#   Defaults to <tt>true</tt>.
+#
 # [*file_pattern*]
 #   String. The pattern that is used to determine which files not to show
-#   diffs for. This value can be configured in hiera using the cdiff::pattern
+#   diffs for. This value can be configured in hiera using the cdiff::file_pattern
 #   key.
-#   Defaults to <tt>\.eyaml</tt>.
+#   Defaults to <tt>false</tt>.
 #
 # [*line_pattern*]
 #   String. The pattern that is used to determine lines in a diff to not
@@ -36,10 +40,11 @@
 #
 #   Defaults to <tt>false</tt>.
 
-class cdiff(
+class sdiff(
   $ensure       = present,
-  $file_pattern = hiera('cdiff::file_pattern',false),
-  $line_pattern = hiera('cdiff::line_pattern',false),
+  $color        = hiera('sdiff::color',true),
+  $file_pattern = hiera('sdiff::file_pattern',false),
+  $line_pattern = hiera('sdiff::line_pattern',false),
 ) {
 
   $file_ensure = $ensure ? {
@@ -47,11 +52,11 @@ class cdiff(
     default => present,
   }
 
-  file { '/usr/local/bin/cdiff':
+  file { '/usr/local/bin/sdiff':
     ensure  => $file_ensure,
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-    content => template('cdiff/cdiff.erb'),
+    content => template('sdiff/sdiff.erb'),
   }
 }
